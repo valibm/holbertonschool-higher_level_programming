8b975dccfc7b41e30eb33bee3013b30d9cc6beed
root <valibm>
2024-01-28 02:35:12 -0800

    Added necessary files

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+s0;136;0c#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    check_multiples = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            check_multiples.append(True)
+        else:
+            check_multiples.append(False)
+    return check_multiples
