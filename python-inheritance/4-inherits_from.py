282053ff1799d1bf702a6aeb919eba50b1246580
root <valibm>
2024-02-11 06:27:45 -0800

    Handled an error

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""
-Checks if the object is an instance of a class
-that inherited from the specified class.
-"""
+"""Defines an inherited class-checking function."""
 
 
 def inherits_from(obj, a_class):
-    """
-    If the object is an instance of a class that inherited from
-    the specified class (directly or indirectly), returns True.
-    Otherwise, returns False.
-    """
+    """Checks if an object is an inherited instance of a class.
 
-    if isinstance(obj, a_class) and type(obj) != a_class:
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
-    else:
-        return False
+    return False
